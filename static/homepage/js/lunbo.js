431e022ba7d9ca84eb7baf0ed8f254cfff5121d4
fanlu <13512102724@163.com>
2018-07-27 00:23:56 +0800

    add app homepage, register, login
    Signed-off-by: fanlu <13512102724@163.com>

---
+++ static/homepage/js/lunbo.js
@@ -0,0 +1,79 @@
+var imgPrc = document.getElementById("prc")
+var content = document.getElementById("list")
+var jsLisArr = content.getElementsByTagName("li")
+var jsLunbo = document.getElementById("luanbo")
+var jsLeft = document.getElementById("le")
+var jsRight = document.getElementById("ri")
+
+jsLisArr[0].style.backgroundColor = "red"
+
+var currentPage = 1
+
+// set time
+var timer = setInterval(startLoop, 1000)
+function startLoop()
+{
+	currentPage++
+	changePage()
+}
+
+function changePage() {
+	if (currentPage == 6) {
+		currentPage = 1;
+	} else if (currentPage == 0) {
+		currentPage = 5
+	}
+	imgPrc.src = "static/homepage/img/img" + currentPage + ".jpg"
+	
+	for (var i = 0; i < jsLisArr.length; i++) {
+		jsLisArr[i].style.backgroundColor = "#aaa"
+	}
+	jsLisArr[currentPage-1].style.backgroundColor="red"
+}
+
+// mouse come in
+jsLunbo.addEventListener("mouseover", overFunc, false)
+function overFunc() {
+	clearInterval(timer)
+	// display bunt
+	jsLeft.style.display = "block"
+	jsRight.style.display = "block"
+	
+}
+jsLunbo.addEventListener("mouseout", outFunc, false)
+function outFunc() {
+	timer = setInterval(startLoop, 1000)
+	
+	//undisplay bunt
+	jsLeft.style.display = "none"
+	jsRight.style.display = "none"
+}
+
+jsLeft.addEventListener("mouseover", deep, false)
+jsRight.addEventListener("mouseover", deep, false)
+function deep() {
+	this.style.backgroundColor = "rgba(0,0,0,0.6)"
+}
+
+jsLeft.addEventListener("mouseout", nodeep, false)
+jsRight.addEventListener("mouseout", nodeep, false)
+function nodeep() {
+	this.style.backgroundColor = "rgba(0,0,0,0.2)"
+}
+
+jsLeft.addEventListener("click", function() {
+	currentPage--
+	changePage()
+}, false)
+jsRight.addEventListener("click", function() {
+	currentPage++
+	changePage()
+}, false)
+
+// come in clicle
+for (var i = 0; i < jsLisArr.length; i++) {
+	jsLisArr[i].addEventListener("mouseover", function(){
+		currentPage = parseInt(this.innerHTML)
+		changePage()
+	},false)
+}
---
+++ static/homepage/js/news.js
@@ -0,0 +1,19 @@
+var zuiXin = document.getElementById("zuixin")
+var qiWen = document.getElementById("qiwen")
+var lvXing = document.getElementById("lvxing")
+
+zuiXin.addEventListener("click", function(){
+	zuiXin.style.backgroundColor = "#6985A5"
+	qiWen.style.backgroundColor = "red"
+	lvXing.style.backgroundColor = "red"
+}, false)
+qiWen.addEventListener("click", function(){
+	qiWen.style.backgroundColor = "#6985A5"
+	zuiXin.style.backgroundColor = "red"
+	lvXing.style.backgroundColor = "red"
+}, false)
+lvXing.addEventListener("click", function(){
+	lvXing.style.backgroundColor = "#6985A5"
+	qiWen.style.backgroundColor = "red"
+	zuiXin.style.backgroundColor = "red"
+}, false)
